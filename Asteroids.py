e7f9a27cfe5a0dc02e0af31f85cb282ef481d2bf
Matt Wilkinson <mattwilk23@ksu.edu>
2018-12-07 15:26:07 -0600

    first commit to github

---
+++ Asteroids.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  5 00:27:12 2018
+
+@author: matt
+"""
+import pygame
+
+screen_w = 800
+screen_h = 600
+
+black = (0,0,0)
+white = (255,255,255)
+
+spaceshipIMG = pygame.image.load('spaceship-off.bmp')
+
+def ship(x,y,angle):
+    turn = pygame.transform.rotate(spaceshipIMG, angle)
+    gameDisplay.blit(turn,(x,y))
+    
+
+    
+x = (screen_w *.45)
+y = (screen_h * .5)
+
+angle = 0
+cangle = 0
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((screen_w,screen_h))
+pygame.display.set_caption('Asteroids')
+clock = pygame.time.Clock()
+
+crashed = False
+
+while not crashed:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                cangle = 3
+            elif event.key == pygame.K_RIGHT:
+                cangle = -3
+                
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                cangle = 0
+                
+    
+    angle += cangle
+    gameDisplay.fill(black)        
+    ship(x,y,angle)   
+    pygame.display.update()
+    clock.tick(60)
+    
+pygame.quit()
---
+++ Asteroids_oo.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 21:24:01 2018
+
+@author: matt
+"""
+
+import pygame
+import numpy as np
+
+class game_object():
+    
+    def __init__(self, image, position, velocity = 0):
+        
+        self.image = image
+        self.position = position[:]
+        self.velocity = velocity
+    
+    def draw(self,screen):
+        screen.blit(self.image,(self.position[0],self.position[1]))
+        
+class Ship(game_object):
+    
+    def __init__(self,position):
+        #initialize ship in the first position
+        super().__init__(pygame.image.load('spaceship-off.bmp'),position)
+        self.angle = 0
+        
+        self.direction = [0,-1]
+        
+    def move(self):
+        
+        pass
+        
+    def draw(self, screen):
+        
+        self.ship_rot = pygame.transform.rotate(self.image,self.angle)
+        
+        screen.blit(self.ship_rot,(self.position[0],self.position[1]))
+    
+    
+
+class missile(game_object):
+    
+    pass
+
+class asteroid(game_object):
+    
+    pass
+
+class Asteroids_Game():
+    
+    def __init__(self):
+        
+        pygame.init()
+        
+        #Initialize Screen
+        self.width = 1500
+        self.height = 1000
+        self.screen = pygame.display.set_mode((self.width,self.height))
+        pygame.display.set_caption('Asteroids')
+        
+        #Colors
+        self.Black = (0,0,0)
+        self.White = (255,255,255)
+               
+        #FPS
+        self.FPS = 50
+       
+        
+        #timer
+        self.clock = pygame.time.Clock()
+        
+        #Initialize Ship Position
+        
+        self.ship = Ship([self.width*.45,self.height*.5])
+        
+   
+    def run(self):
+        
+        running = True
+        
+        while running:
+            
+            for event in pygame.event.get():
+                
+                if event.type == pygame.QUIT:
+                    running = False
+                    
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        
+                        self.ship.angle += 10
+                        self.ship.angle %= 360
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.angle  -=  10
+                        
+                    if event.key == pygame.K_UP:
+                        pass
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                        
+                        self.ship.angle += 0
+                        
+            #Game Boundaries
+ #           if self.x_change == 5:
+  #              if self.x == self.width:
+   #                 self.x = -45
+    #            else:
+     #               self.x += self.x_change
+      #      if self.x_change == -5:
+       #         if self.x == -30:
+        #            self.x = self.width
+         #       else:
+          #          self.x += self.x_change
+                
+                
+                self.update_all()
+                
+    
+        pygame.quit()
+
+    def update_all(self):
+        
+        self.screen.fill(self.Black)  
+        self.ship.draw(self.screen)
+        pygame.display.update()
+        self.clock.tick(self.FPS)
+        
+    
